d7a50a9caf8b37ec1e9433b212f749ae383e4f73
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2016-11-03 11:37:40 -0500

    Updaint assignment 9

---
+++ Exercise2/scripts/main.js
@@ -0,0 +1,22 @@
+/*******Part 1***********/
+//Place all of your part 1 code here
+
+
+
+
+/*******Part 2***********/
+var foo = "This is a String";
+
+
+
+/*******Part 3***********/
+
+
+
+/*******Part 4***********/
+var fName = "John"
+var lName = "Doe"
+
+
+
+
