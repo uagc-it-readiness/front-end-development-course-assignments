47b7c46e1e4210d1857583ba18f67c80cafc7787
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2016-11-07 16:59:09 -0600

    Adding Early Assignment 11

---
+++ Assignment11/JS-Assignment/scripts/questions.js
@@ -0,0 +1,42 @@
+/////////
+//Question 1
+//What is the value of x?
+var arr = ['User', 'Email', 'Phone', 'Address', 'Zip'];
+var x = arr[2];
+//Answer:
+//x = ????
+///
+
+
+/////////
+//Question 2
+//What is the value of y?
+var str = "Hello World";
+var y = str.indexOf('W');
+//Answer:
+//y = ????
+///
+
+/////////
+//Question 3
+//What is the value of z?
+var str = "Hello World";
+var z = str.indexOf('h');
+//Answer:
+//z = ????
+///
+
+
+/////////
+//Question 4
+//What is the value of a?
+var bo = 'true'; 
+var a = (bo === true);
+//Answer:
+//a = ????
+///
+
+
+
+
+
