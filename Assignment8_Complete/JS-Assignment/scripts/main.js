3c80097b929c8ea6863de6981d3c5a4fd313f2af
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2016-11-08 12:17:07 -0600

    Adding assignment 8 complete

---
+++ Assignment8_Complete/JS-Assignment/scripts/main.js
@@ -0,0 +1,11 @@
+var dayOfWeek;
+var cupsOfCoffee;
+var isWearingGreen;
+
+dayOfWeek = "Monday"
+cupsOfCoffee = 15;
+isWearingGreen = false;
+
+console.log(dayOfWeek);
+console.log(cupsOfCoffee);
+console.log(isWearingGreen);
