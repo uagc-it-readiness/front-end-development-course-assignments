52adabec2f53076cb360ab511a2bf07c3991ba93
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2016-12-01 16:42:45 -0600

    Removing code

--- Assignment16/scripts/main.js
+++ Assignment16/scripts/main.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
 
-     ///// Code goes here
-     $('#btnOne').on('click', function(){
-         alert('hello');
-     });
-
-
-    $('#btnOne').on('click', sayhello);
-    
-    function sayhello(){
-        alert('hello');
-    }
-
 
 
 });
