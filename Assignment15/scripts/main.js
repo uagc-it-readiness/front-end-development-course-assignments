7061669a3f8b85b9e74c447894776f4ccdb9d114
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2016-12-01 17:19:25 -0600

    Fixing

--- Assignment15/scripts/main.js
+++ Assignment15/scripts/main.js
@@ -1,17 +1,6 @@
 $(document).ready(function(){
 
-     ///// Code goes here
-     $('#btnOne').on('click', function(){
-         alert('hello');
-     });
-
-
-    $('#btnOne').on('click', sayhello);
     
-    function sayhello(){
-        alert('hello');
-    }
-
 
 
 });
