bae49082585420640b5673fe9c3cf9fe3b2cd412
Daniel Worthy <danieljworthy@gmail.com>
2016-12-08 05:05:38 -0600

    Premessing it up

--- Assignment18/scripts/main.js
+++ Assignment18/scripts/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    $('input').on('blur', function () {
+    $('input, textarea').on('blur', function () {
         var input = $(this);
-        validateInput(input)
+        validateRequiredInput(input)
 
-        if(input.data('isEmail') === 'true'){
+        if (input.data('isemail') === true) {
             validateEmail(input);
         }
 
-        if (input.attr('type') === 'password'){
+        if (input.attr('type') === 'password') {
             validatePassword();
         }
     });
@@ -15,11 +15,27 @@ $(document).ready(function () {
     $('#btnSubmit').on('click', function () {
         validateAllInputs();
     });
+
+    $('#btnReset').on('click', function () {
+        var allInputs = $('input, textarea')
+        allInputs.val('');
+        allInputs.removeClass('invalid');
+        $('.error').hide();
+        $('#messages').html('');
+    });
+
+
 });
 
-function validateInput(input) {
+/**
+ * Validates a single input.
+ * Accepts a jQuery object for it's input
+ * @param {any} input
+ */
+function validateRequiredInput(input) {
     var val = input.val();
 
+    //Find the required input span.
     var msgSpan = input.closest('.input-group').find('.requiredMsg');
     if (val === '') {
         msgSpan.fadeIn();
@@ -30,30 +46,62 @@ function validateInput(input) {
     }
 }
 
+/**
+ * Validates an email input
+ * Accepts a jQuery object as its input parameter
+ * @param {any} email
+ */
 function validateEmail(email) {
     var emailValue = email.val();
 
     var emailErrorMsg = email.closest('.input-group').find('.emailMsg');
-    if (emailValue !== '' && emailValue.indexOf('@') > -1 && emailValue.indexOf('@') > -1) {
-        emailErrorMsg.fadeOut();
-        email.removeClass('invalid');
-    } else {
-        emailErrorMsg.fadeIn();
-        email.addClass('invalid');
+    
+    //Only validate if it is filled out
+    if (emailValue !== '') {
+        if (emailValue.indexOf('@') > -1 && emailValue.indexOf('@') > -1) {
+            emailErrorMsg.fadeOut();
+            email.removeClass('invalid');
+        } else {
+            emailErrorMsg.fadeIn();
+            email.addClass('invalid');
+        }
     }
 
 }
 
-function validatePassword(){
+/**
+ * Compares the inputs  and makes sure they match.
+ */
+function validatePassword() {
     var password = $('#password');
+    var passwordValue = password.val();
     var passwordConfirm = $('#passwordConfirm');
-    
+    var passwordConfirmValue = passwordConfirm.val();
+
+    //Only validate if both are filled out
+    if (passwordValue !== '' && passwordConfirmValue !== '') {
+
+        var passwordErrorMsg = password.closest('.input-group').find('.matchingMsg');
+
+        if (passwordValue !== passwordConfirmValue) {
+            passwordErrorMsg.show();
+            password.addClass('invalid');
+            passwordConfirm.addClass('invalid');
+        } else {
+
+            passwordErrorMsg.hide();
+            password.removeClass('invalid');
+            passwordConfirm.removeClass('invalid');
+ 
+        }
+
+    }
 }
 
 
 
 function validateAllInputs() {
-    var allInputs = $('input');
+    var allInputs = $('input, textarea');
 
     for (var i = 0; i < allInputs.length; i++) {
         validateInput(allInputs.eq(i));
@@ -68,101 +116,4 @@ function validateAllInputs() {
         $('.form-elements').fadeOut();
     }
 
-}
-
-
-
-
-
-
-
-
-
-var btn = getE('btnSubmit');
-btn.addEventListener('click', submit);
-
-var btn2 = getE('btnReset');
-btn2.addEventListener('click', reset);
-
-var inputs = ['firstName', 'lastName', 'emailAddress', 'password', 'passwordConfirm', 'about'];
-var ERROR_CLASS = 'invalid';
-
-function submit() {
-    var elm;
-    var errorList = [];
-
-    //loop through the array of inputs from line 7
-    for (var i = 0; i < inputs.length; i++) {
-        //Get the element from the corresponding item in the array.
-        elm = getE(inputs[i]);
-
-
-        if (elm.value === '') {
-            errorList.push(elm.placeholder + ' is required');
-            elm.classList.add(ERROR_CLASS);
-        } else {
-            elm.classList.remove(ERROR_CLASS);
-        }
-
-    }
-
-    var passWord = getE('password').value;
-    var passWordConfirm = getE('passwordConfirm').value;
-    if (passWord !== passWordConfirm) {
-        errorList.push('Passwords do not match');
-    }
-
-    var email = getE('emailAddress');
-    if (email.value.indexOf('@') == -1) {
-        errorList.push('Enter a valid Email');
-    }
-
-    var tim = getE('timezone');
-    if (tim.value === '') {
-        tim.classList.add(ERROR_CLASS);
-        errorList.push('Timezone is required');
-    } else {
-        tim.classList.remove(ERROR_CLASS);
-    }
-
-    var terms = getE('terms');
-    if (terms.checked === false) {
-        errorList.push('You must accept the terms and conditions');
-    }
-
-
-
-
-    var html = '';
-    if (errorList.length > 0) {
-        //Loop through and add each message to our html variable
-        html = '<ul class="errors">';
-        for (var i = 0; i < errorList.length; i++) {
-            //html += errorMsg[i] + '<br />';            
-            html = html + '<li>' + errorList[i] + '</li>';
-        }
-        html += "</ul>";
-    } else {
-        html = '<h1>Thank you for registering</h1>';
-        reset();
-    }
-
-    getE('messages').innerHTML = html;
-}
-
-function reset() {
-    for (var i = 0; i < inputs.length; i++) {
-        var elm = getE(inputs[i])
-        elm.value = '';
-        elm.classList.remove(ERROR_CLASS);
-    }
-
-    getE('messages').innerHTML = '';
-
-    getE('timezone').value = '';
-    getE('terms').checked = false;
-}
-
-function getE(id) {
-    return document.getElementById(id);
 }
