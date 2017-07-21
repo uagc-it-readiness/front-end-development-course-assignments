17a5b01dea4e6638d60423d8b2fde4e9dc7ddd4b
Daniel Worthy <daniel.worthy@rockfishinteractive.com>
2017-07-21 14:46:04 -0500

    adding website template

---
+++ website-template/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+
+//We are adding sass as a gulp dependancy. It will run 'sass' before it starts the browser sync. 
+//This makes sure that we have the latest CSS.
+gulp.task('browserSync',  function () {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    })
+});
+
+//Starts up a dev server for us
+//It also watches files and reloads the browser when they change.
+gulp.task('dev', ['browserSync'], function () {
+    gulp.watch('src/*.html', browserSync.reload);
+});
